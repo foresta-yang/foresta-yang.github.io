14427ed6834f9542342d8d705d908247b4e5a211
itachi <1430061275@qq.com>
2023-12-18 23:22:30 +0800

    Site updated: 2023-12-18 23:22:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/16/c语言混淆技术/","2023/12/16/hello-world/"];function toRandomPost(){
+var posts=["2023/12/18/LineVD-Statement-level-Vulnerability-Detection-using-Graph-Neural-Networks/","2023/12/16/c语言混淆技术/","2023/12/16/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
